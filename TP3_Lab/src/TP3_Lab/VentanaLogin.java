1c0b157a304b8e8cbe921c87ffed5b658703692a
PabloMaidana <pablomaid1@gmail.com>
2024-03-27 17:58:28 -0300

    2

---
+++ TP3_Lab/src/TP3_Lab/VentanaLogin.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TP3_Lab;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author pablo
+ */
+public class VentanaLogin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("ventana");
+        
+        ventana.setSize(300, 300);       
+
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        ventana.setVisible(true);
+    }
+    
+}
